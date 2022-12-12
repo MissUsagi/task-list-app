b8bec2f0021dbe920927472cd160b05473b0cf9a
MissUsagi <syljanko@gmail.com>
2022-12-12 16:21:56 +0100

    dodano nowe testy

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -13,10 +13,34 @@
 
 import { shallowMount } from '@vue/test-utils'
 import BaseHeader from '@/components/ui/BaseHeader.vue'
+import TaskList from '@/components/pages/TaskList.vue'
 
-describe('BaseHeader.vue', () => {
-  const wrapper = shallowMount(BaseHeader)
-  it('data jest widoczna', () => {
-    expect(wrapper.find("h1").text()).toBe("Dzisiaj jest 11/12");
-  })
-})
+describe('BaseHeader h1 test', () => {
+  const wrapper = shallowMount(BaseHeader);
+  const data = new Date();
+  const dataString = "Dzisiaj jest " + data.getDate() + '/' + (data.getMonth() + 1);
+  it('dzisiaj jest 12/12', () => {
+    expect(wrapper.find("h1").text()).toBe("Dzisiaj jest 12/12");
+  });
+  it('wyswietla dzisiejsza date', () => {
+    expect(wrapper.find("h1").text()).toBe(dataString)
+  });
+
+});
+
+describe('New task is added', () => {
+  const wrapper = shallowMount(TaskList);
+  const testTask = {
+    category: "Zdrowie",
+    title: "Wizyta u lekarza",
+    description: "Zabrać PESEL",
+    localization: "ul. Lekarska 6",
+    time: "15:16",
+  };
+  wrapper.vm.addTask(testTask);
+
+  it('task dodano pomyślnie na poczatku tablicy', () => {
+    expect(wrapper.vm.myTasks[0]).toEqual(testTask);
+  });
+}
+)
